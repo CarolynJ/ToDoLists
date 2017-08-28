c60930cccc4171d97e9169ce81ddcb9d157a9155
Carolyn Jillson <carolynj70@gamil.com>
2017-08-28 17:30:39 -0400

    fixed bug so list items don't disappear

--- Scripts/JavaScript.js
+++ Scripts/JavaScript.js
@@ -1,31 +1,34 @@
 ﻿$(document).ready(function () {
     $("#add1").click(function () {
-        var task = $("input[name=firstListItem]").val();
-        $("#firstList").append("<li>" + task + "</li>");
+        var task = $("input[name=firstListItem]").val()
+        $("#firstList").append("<li>" + task + "</li>")
          $("input[name=firstListItem]").val("");
     });
     $("#add2").click(function () {
-        var task2 = $("input[name=thornListItem]").val();
+        var task2 = $("input[name=thornListItem]").val()
         $("#thornList").append("<li>" + task2 + "</li>");
         $("input[name=thornListItem]").val("");
     });
 
     $("input[name=firstListItem]").keyup(function (event) {
         if (event.keyCode === 13) {
-            var task = $("input[name=firstListItem]").val();
+            var task = $("input[name=firstListItem]").val()
             $("#firstList").append("<li>" + task + "</li>");
         }
     });
 
     $("input[name=thornListItem]").keyup(function (event) {
         if (event.keyCode === 13) {
-            var task2 = $("input[name=thornListItem]").val();
+            var task2 = $("input[name=thornListItem]").val()
             $("#thornList").append("<li>" + task2 + "</li>");
             $("input[name=firstListItem]").val("");
         }
     });
     $(document).on("dblclick", "li", function () {
         $(this).toggleClass("strike");
+    });
+    $(function () {
+        $("ol.list").sortable();
     });
  });
 
